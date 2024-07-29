a58ac15fed1a1741b17ca9cb6f95d0787a8fbd8f
anuradhakumari2025 <krianuradha4321@gmail.com>
2024-07-29 10:03:04 +0530

    Now time change every second

--- src/components/PresentTime.jsx
+++ src/components/PresentTime.jsx
@@ -1,5 +1,31 @@
+/*
 let PresentTime = () =>{
   let time = new Date();
-  return <p className="lead">The current time is: {time.toLocaleDateString()} = {time.toLocaleDateString()} </p>
+  return <p className="lead">The current time is: {time.toLocaleDateString()} = {time.toLocaleTimeString()} </p>
 }
+export default PresentTime;
+*/
+//The above code only show present time and date and on refresh will change
+
+import { useEffect, useState } from "react";
+
+let PresentTime = () => {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+  return (
+    <p className="lead">
+      The current time is: {time.toLocaleDateString()} ={" "}
+      {time.toLocaleTimeString()}{" "}
+    </p>
+  );
+};
 export default PresentTime;
